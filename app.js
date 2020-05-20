7ca6db7087f4fe98802e6a7bf05a39bd26cc7c30
Abim <apexabim09@gmail.com>
2020-05-20 11:59:24 +0545

    added new functionalities

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let counter = 0;
 let sCounter = document.getElementById('count');
 
-let btnPressed = false;
+var btnPressed = false;
 
-let btn = document.getElementById('btnStart');
+let btnStart = document.getElementById('btnStart');
+let btnReset = document.getElementById('btnReset');
 let time = document.getElementById("timer");
+
 
 var seconds = 60;
 var timer;
@@ -13,10 +15,9 @@ function myFunction() {
     if (seconds < 60) {
         time.innerHTML = "00:" + seconds;
     }
-      if (seconds < 10) {
+    if (seconds < 10) {
         time.innerHTML = "00:0" + seconds;
     }
-    
     if (seconds >= 0) {
         seconds--;
         if (seconds < 0) {
@@ -27,18 +28,34 @@ function myFunction() {
     }
 }
 
+btnReset.onclick = () => {
+    location.reload();
+}
 
+function timeOut() {
+    setTimeout(function() { location.reload(); }, 10000);
+}
 
-
-btn.onclick = () => {
+btnStart.onclick = () => {
     btnPressed = true;
     if (!timer) {
         timer = window.setInterval(function() {
             myFunction();
             if (seconds < 0 && counter >= 400) {
-                window.alert('CONGRATULATIONS.....');
+                winnerResult = 'CONGRATULATIONS. You pressed the spacebar ' + counter + ' times in a minute. ';
+                infoResult = 'Game will restart in 10 seconds.';
+
+                result.innerHTML = winnerResult;
+                info.innerHTML = infoResult;
+                timeOut();
             } else if (seconds < 0 && counter < 400) {
-                window.alert('SORRY.....');
+                loserResult = 'SORRY! You pressed the spacebar only ' + counter + ' times. ';
+                infoResult = 'Game will restart in 10 seconds.';
+
+                result.innerHTML = loserResult;
+                info.innerHTML = infoResult;
+                timeOut();
+
             }
         }, 1000);
 
@@ -47,11 +64,16 @@ btn.onclick = () => {
                 counter += 1;
                 sCounter.innerHTML = counter;
             }
-        })
-
+        });
     }
-
 }
 
 
 document.getElementById("timer").innerHTML = "01:00";
+
+
+//Get full year
+let year = document.getElementById('fullYear');
+let date = new Date();
+
+year.innerHTML = date.getFullYear();
